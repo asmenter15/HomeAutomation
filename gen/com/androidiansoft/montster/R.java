d9b8cc5cf4bf9cbc5fa8546de86d9b8a735b9cac
Aaron <asmenter15@gmail.com>
2013-12-29 23:28:33 -0700

    - finally got wifi lights working sloppily

--- gen/com/androidiansoft/montster/R.java
+++ gen/com/androidiansoft/montster/R.java
@@ -16,8 +16,9 @@ public final class R {
     }
     public static final class id {
         public static final int datesList=0x7f070000;
-        public static final int menu_settings=0x7f070005;
-        public static final int pic_viewer=0x7f070004;
+        public static final int light=0x7f070004;
+        public static final int menu_settings=0x7f070006;
+        public static final int pic_viewer=0x7f070005;
         public static final int title=0x7f070001;
         public static final int viewCam=0x7f070002;
         public static final int viewDates=0x7f070003;
@@ -33,6 +34,8 @@ public final class R {
     public static final class string {
         public static final int app_name=0x7f040000;
         public static final int hello_world=0x7f040001;
+        public static final int light_off=0x7f040008;
+        public static final int light_on=0x7f040007;
         public static final int menu_settings=0x7f040002;
         public static final int security=0x7f040006;
         public static final int title_activity_main_screen=0x7f040003;
--- src/com/androidiansoft/montster/MainScreen.java
+++ src/com/androidiansoft/montster/MainScreen.java
@@ -1,9 +1,25 @@
 package com.androidiansoft.montster;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.http.NameValuePair;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.ResponseHandler;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.BasicResponseHandler;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+
 import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
@@ -12,6 +28,9 @@ public class MainScreen extends Activity {
 
 	Button viewList;
 	Button viewCam;
+	Button light;
+	SharedPreferences preferences;
+	public static final String LIGHT_PREF = "light_pref";
 	
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -19,6 +38,22 @@ public class MainScreen extends Activity {
         setContentView(R.layout.main_layout);
         viewList = (Button) findViewById(R.id.viewDates);
         viewCam = (Button) findViewById(R.id.viewCam);
+        light = (Button) findViewById(R.id.light);
+        
+		preferences = PreferenceManager
+				.getDefaultSharedPreferences(this
+						.getApplicationContext());
+		
+		int lightPref = preferences.getInt(LIGHT_PREF, 0);
+		
+		if(lightPref == 0) {
+			light.setText(R.string.light_off);
+		} else if(lightPref == 1) {
+			light.setText(R.string.light_on);
+		} else {
+			changeLights();
+		}
+        
         viewList.setOnClickListener(new OnClickListener() {
 		
 			public void onClick(View v) {
@@ -36,5 +71,65 @@ public class MainScreen extends Activity {
 				
 			}
 		});
+        
+        light.setOnClickListener(new OnClickListener() {
+			
+			public void onClick(View v) {
+				changeLights();
+			}
+		});
     }
+    
+    private void changeLights() {
+    	
+    	AsyncTask<String, Void, String> task = new AsyncTask<String, Void, String>() {
+
+
+			@Override
+			protected String doInBackground(String... params) {
+				
+				DefaultHttpClient httpClient = new DefaultHttpClient();
+				HttpPost get = new HttpPost("http://68.110.111.35:8080");
+				
+				List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(1);
+				nameValuePairs
+						.add(new BasicNameValuePair("$", "l"));
+
+				String response = "";
+				try {
+					ResponseHandler<String> responseHandler=new BasicResponseHandler();
+					get.setEntity(new UrlEncodedFormEntity(nameValuePairs));					
+					response = httpClient.execute(get, responseHandler);
+					System.out.println(response);
+				} catch (ClientProtocolException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				
+				return response;
+			}
+			
+			@Override
+			protected void onPostExecute(String result) {
+				SharedPreferences.Editor editor = preferences.edit();
+				if(result.trim().equals("1")) {
+					light.setText(R.string.light_on);
+					editor.putInt(LIGHT_PREF, 1);
+					editor.commit();
+				} else if (result.trim().equals("0")) {
+					light.setText(R.string.light_off);
+					editor.putInt(LIGHT_PREF, 0);
+					editor.commit();
+				}
+			}
+    		
+    		
+    		
+		};
+		task.execute("");
+    }
+    
 }
